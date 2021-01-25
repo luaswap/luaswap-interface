947a233e5041069c75d9b27f9925f44ee64bc575
tinhbeng <tinhbeng1092@gmail.com>
2021-01-25 10:55:23 +0700

    Update listToken

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,12 +1,12 @@
 // the LuaSwap Default token list lives here
-// const COMMIT_HASH = 'ddffb9680613a0a2fc860f2411c602d971d9a3b3'
+const COMMIT_HASH = 'ddffb9680613a0a2fc860f2411c602d971d9a3b3'
 
-// export const DEFAULT_TOKEN_LIST_URL =
-//   'https://raw.githubusercontent.com/tomochain/luaswap-token-list/' +
-//   COMMIT_HASH +
-//   '/build/luaswap-default.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://raw.githubusercontent.com/tomochain/luaswap-token-list/' +
+  COMMIT_HASH +
+  '/build/luaswap-default.tokenlist.json'
 
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/tomochain/luaswap-interface/Tinh/public/tomochain.tokenlist.json'
+// export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/tomochain/luaswap-interface/Tinh/public/tomochain.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
 
