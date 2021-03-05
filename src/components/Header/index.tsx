b80c0176610f6670af7a0b6fd5a09c015fe2f65b
tinhbeng <tinhbeng1092@gmail.com>
2021-03-05 17:11:19 +0700

    Add gasLimit - makerConvert

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -233,7 +233,7 @@ const HideSmall = styled.div`
     width: 200px;
     padding: 10px;
     background-color: ${({ theme }) => theme.bg1};
-    color: #c3a56e 
+    color: #c3a56e;
     a {
       color: #ecb34b;
     }
