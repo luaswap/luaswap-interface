f55df7cf92bcdfc484a2052442d9f88ec9e6685a
PhucNH <phucnh.dev@gmail.com>
2021-11-06 11:43:13 +0700

    upgrade version default token list

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -5,7 +5,7 @@ import { hex } from 'wcag-contrast'
 import { Token, ChainId } from '@luaswap/sdk'
 import { IsTomoChain } from '../utils'
 
-const commit_hash = 'a2d53ddef0f76f2d5340ddd6bd5899d6012898d0'
+const commit_hash = 'b5bcf504a826957df81af83e602973db366ed1a0'
 
 async function getColorFromToken(token: Token): Promise<string | null> {
   if (token.chainId === ChainId.RINKEBY && token.address === '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735') {
